6cef51cab3fbad540dabda89f9d5a2e0b1f50890
Omar Morales <morales.ortega.omar@gmail.com>
2020-10-22 21:11:41 -0500

    Adding beer repository

---
+++ src/main/java/omar/springframework/msscbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,9 @@
+package omar.springframework.msscbeerservice.repositories;
+
+import omar.springframework.msscbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository <Beer, UUID>{
+}
